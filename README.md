997f3e05793695aa254de35a6fb4324b15a901f7
Piyush Yadav <piyushyadav0191@gmail.com>
2022-11-05 08:51:31 +0530

    Server

---
+++ README.md
@@ -0,0 +1,14 @@
+# to get started
+
+# download the code 
+- cd `folder-name`
+
+- npm install
+
+# for client
+- npm run start
+
+# for server
+- npm run server:dev
+
+Dont forget to add .env
---
+++ src/pages/Main/Home.js
@@ -0,0 +1,135 @@
+import React, { useState } from 'react';
+import { v4 as uuidV4 } from 'uuid';
+import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+
+const Home = () => {
+    const navigate = useNavigate();
+
+    const [roomId, setRoomId] = useState('');
+    const [username, setUsername] = useState('');
+
+    const particlesInit = async (main) => {
+        await loadFull(main);
+      };
+    
+
+    const createNewRoom = (e) => {
+        e.preventDefault();
+        const id = uuidV4();
+        setRoomId(id);
+        toast.success('Created a new room');
+    };
+
+    const joinRoom = () => {
+        if (!roomId || !username) {
+            toast.error('ROOM ID & username is required');
+            return;
+        }
+
+        // Redirect
+        navigate(`/editor/${roomId}`, {
+            state: {
+                username,
+            },
+        });
+    };
+
+    const handleInputEnter = (e) => {
+        if (e.code === 'Enter') {
+            joinRoom();
+        }
+    };
+    return (
+        <>
+         <Particles
+      id="tsparticles"
+      init={particlesInit}
+      options={{
+        "fullScreen": {
+            "enable": true,
+            "zIndex": 1
+        },
+        "particles": {
+            "number": {
+                "value": 10,
+                "density": {
+                    "enable": false,
+                    "value_area": 800
+                }
+            },
+            "color": {
+                "value": "#39FCFD"
+            },
+            "shape": {
+                "type": "circle",
+                "image": {
+                    "src": "https://raw.githubusercontent.com/piyushyadav0191/Mall-for-house/main/awe.png"
+                }
+            },
+            "size": {
+                "value": 10,
+                "random": false,
+                "anim": {
+                    "enable": false,
+                    "speed": 40,
+                    "size_min": 0.1,
+                    "sync": false
+                }
+            },
+            "move": {
+                "enable": true,
+                "speed": 4,
+                "direction": "none",
+                "random": false,
+                "straight": false,
+                "out_mode": "out",
+            }
+        },
+    }}
+    />
+ 
+        <div className="homePageWrapper">
+            <div className="formWrapper">
+                <h4 className="mainLabel">Paste invitation ROOM ID</h4>
+                <div className="inputGroup">
+                    <input
+                        type="text"
+                        className="inputBox"
+                        placeholder="ROOM ID"
+                        onChange={(e) => setRoomId(e.target.value)}
+                        value={roomId}
+                        onKeyUp={handleInputEnter}
+                    />
+                    <input
+                        type="text"
+                        className="inputBox"
+                        placeholder="USERNAME"
+                        onChange={(e) => setUsername(e.target.value)}
+                        value={username}
+                        onKeyUp={handleInputEnter}
+                    />
+                    <button className="btn joinBtn" onClick={joinRoom}>
+                        Join
+                    </button>
+                    <span className="createInfo">
+                        If you don't have an invite then create &nbsp;
+                        <a
+                            onClick={createNewRoom}
+                            href=""
+                            className="createNewBtn"
+                        >
+                            new room
+                        </a>
+                    </span>
+                </div>
+            </div>
+            
+        </div>
+        </>
+    );
+};
+
+export default Home;
